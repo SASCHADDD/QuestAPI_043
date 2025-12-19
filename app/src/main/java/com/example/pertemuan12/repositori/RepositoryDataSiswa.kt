2d35d29042de56ac3cbd3767aad22946ecc7f091
SASCHADDD <saschadanu23@gmail.com>
2025-12-19 10:31:50 +0700

    membuat class dan menambahkan variable

--- app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
@@ -1,9 +1,16 @@
 package com.example.pertemuan12.repositori
 
 import com.example.pertemuan12.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
 
+class JaringanRepositpryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa>  = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.getSiswa(dataSiswa)
+}
