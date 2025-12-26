d4a20ad4486a577923582ccdfaaba28043ad25b7
SASCHADDD <saschadanu23@gmail.com>
2025-12-26 22:58:28 +0700

    membuat fungsi home screen

--- app/src/main/java/com/example/pertemuan12/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan12/view/HalamanHome.kt
@@ -1,2 +1,19 @@
 package com.example.pertemuan12.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+//    edit 1.1 : tambahkan parameter navigateToItemEntry
+    navigateToItemEntry: () -> Unit,
+//    edit 2.4 : tambahkan parameter navigateToItemUpdate
+    navigateToItemUpdate:(Int) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
